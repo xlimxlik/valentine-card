e8c1133596c3378e3e177eaa20bc2e0b3b4c9d0e
ALI <105070602+xlimxlik@users.noreply.github.com>
2026-02-09 17:50:47 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
-const music = document.getElementById('bg-music');
-let musicStarted = false;
-
-document.body.addEventListener('click', () => {
-  if (!musicStarted) {
-    musicStarted = true;
-    music.volume = 0; // начинаем с тишины
-    music.play();
-
-    // плавное увеличение громкости
-    let vol = 0;
-    const fadeIn = setInterval(() => {
-      if (vol < 1) {
-        vol += 0.01;
-        music.volume = vol;
-      } else {
-        clearInterval(fadeIn);
-      }
-    }, 100); // каждые 0.1 сек +0.01 к громкости
-  }
-});
-
+onload = () =>{
+    document.body.classList.remove("container");
+};
