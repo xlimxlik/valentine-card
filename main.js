4cef32967edf603435f489f0bf5ebef09b4b9708
ALI <105070602+xlimxlik@users.noreply.github.com>
2026-02-10 12:19:30 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-onload = () =>{
-    document.body.classList.remove("container");
-};
+$(document).ready(function() {
+  const envelope = $("#envelope");
+  
+  let isOpen = false;
+  
+  envelope.on('click', function() {
+    if (isOpen) {
+      envelope.addClass("close").removeClass("open");
+    } else {
+      envelope.addClass("open").removeClass("close");
+    }
+    isOpen = !isOpen;
+  });
+});
+
+
